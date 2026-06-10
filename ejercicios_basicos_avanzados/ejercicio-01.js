fbbf57bdd3aac7e040e029d8cc7cb58b81b2de44
Sheila Aberas <sheila.aberas@gmail.com>
2026-06-10 13:09:14 +0200

    Este es el primer ejercicio

--- ejercicios_basicos_avanzados/ejercicio-01.js
+++ ejercicios_basicos_avanzados/ejercicio-01.js
@@ -1,3 +1,41 @@
-// 1. Define tu superhéroe favorito:
+// Ejercicio 1
 
-// Crea una variable llamada miSuperheroeFavorito y asigna el valor "Hulk".
+
+
+/* 1. Define tu superhéroe favorito:
+
+Crea una variable llamada miSuperheroeFavorito y asigna el valor "Hulk". */
+let miSuperheroeFavorito = "Hulk";
+console.log(miSuperheroeFavorito);
+
+
+/* 2. Define un número:
+
+Crea una variable llamada numeroFavorito y asigna el valor 50. */
+let numeroFavorito = 50;
+console.log(numeroFavorito);
+
+
+// 3. Define las dimensiones:
+
+// Crea una variable llamada altura y asigna el valor 5. 
+let altura = 5;
+console.log(altura);
+
+// Crea una variable llamada longitud y asigna el valor 10.
+let longitud = 10;
+console.log(longitud);
+
+// 4. Calcula la suma:
+
+// Crea una variable llamada suma y asigna el valor de altura + longitud.
+let suma;
+suma = altura + longitud;
+console.log(suma);
+
+
+/* 5. Imprime los resultados:
+
+Muestra todas las variables por consola. */
+
+ 
